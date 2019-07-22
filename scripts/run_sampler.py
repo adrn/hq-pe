23ece1b365e129899cc5214fc13e72e364689f46
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-07-22 15:42:39 -0400

    metallicity bins

--- scripts/run_sampler.py
+++ scripts/run_sampler.py
@@ -28,6 +28,10 @@ teff_step = 300
 teff_binsize = 1.5 * teff_step
 teff_bincenters = np.arange(3400, 7000+1e-3, teff_step)
 
+mh_step = 0.1
+mh_binsize = 2 * mh_step
+mh_bincenters = np.arange(-1.0, 0.3+1e-3, mh_step)
+
 
 def main_rg(pool, overwrite=False):
     # Config stuff:
@@ -37,10 +41,6 @@ def main_rg(pool, overwrite=False):
     metadata = get_metadata()
     rg_mask = get_rg_mask(metadata['TEFF'], metadata['LOGG'])
     metadata = metadata[rg_mask]
-
-    # Make sure paths exist:
-    for path_ in [cache_path, plot_path]:
-        os.makedirs(path_, exist_ok=True)
 
     for i, ctr in enumerate(logg_bincenters):
         l = ctr - logg_binsize / 2
@@ -57,6 +57,8 @@ def main_rg(pool, overwrite=False):
 
 
 def main_ms(pool, overwrite=False):
+    """Run main sequence in bins of TEFF"""
+
     # Config stuff:
     name = 'ms'
 
@@ -65,14 +67,34 @@ def main_ms(pool, overwrite=False):
     ms_mask = get_ms_mask(metadata['TEFF'], metadata['LOGG'])
     metadata = metadata[ms_mask]
 
-    # Make sure paths exist:
-    for path_ in [cache_path, plot_path]:
-        os.makedirs(path_, exist_ok=True)
-
     for i, ctr in enumerate(teff_bincenters):
         l = ctr - teff_binsize / 2
         r = ctr + teff_binsize / 2
         pixel_mask = ((metadata['TEFF'] > l) & (metadata['TEFF'] <= r))
+
+        # Load samples for this bin:
+        logger.debug("{} {}: Loading samples".format(name, i))
+        ez_samples = get_ez_samples(metadata['APOGEE_ID'][pixel_mask])
+
+        # Run
+        run_pixel(name, i, ez_samples, cache_path, plot_path, pool,
+                  nwalkers=80)
+
+
+def main_ms_mh(pool, overwrite=False):
+    """Run main sequence in bins of [M/H]"""
+    # Config stuff:
+    name = 'ms-mh'
+
+    # Load all data:
+    metadata = get_metadata()
+    ms_mask = get_ms_mask(metadata['TEFF'], metadata['LOGG'])
+    metadata = metadata[ms_mask]
+
+    for i, ctr in enumerate(mh_bincenters):
+        l = ctr - mh_binsize / 2
+        r = ctr + mh_binsize / 2
+        pixel_mask = ((metadata['M_H'] > l) & (metadata['M_H'] <= r))
 
         # Load samples for this bin:
         logger.debug("{} {}: Loading samples".format(name, i))
@@ -98,22 +120,21 @@ if __name__ == '__main__':
                         action="store_true",
                         help="Overwrite stuff.")
 
-    parser.add_argument("-n", "--name", dest="name", required=True, type=str,
-                        help="Name; must be either 'ms' or 'rg'")
+    parser.add_argument("-n", "--name", dest="name", required=True,
+                        choices=['ms', 'rg', 'ms_mh'])
 
     args = parser.parse_args()
 
     pool = choose_pool(mpi=args.mpi)
+
+    # Make sure paths exist:
+    for path_ in [cache_path, plot_path]:
+        os.makedirs(path_, exist_ok=True)
+
+    func = eval('main_{}'.format(args.name))
+
     try:
-        if args.name == 'ms':
-            main_ms(pool=pool, overwrite=args.overwrite)
-
-        elif args.name == 'rg':
-            main_rg(pool=pool, overwrite=args.overwrite)
-
-        else:
-            logger.error("Invalid name '{}'".format(args.name))
-
+        func(pool=pool, overwrite=args.overwrite)
         pool.close()
     except Exception as e:
         pool.close()
